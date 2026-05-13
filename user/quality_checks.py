90dd58bbfe51358c6b3c69f8de54b0ba92e77eb3
nipunkhanderia <nipunkhanderia@gmail.com>
2026-05-13 21:32:16 +0000

    added quality checks

---
+++ user/quality_checks.py
@@ -0,0 +1,16 @@
+
+
+def api_response_is_not_null(response):
+    cleaned_response = response.strip()
+    if len(cleaned_response)>0:
+        print("we have valid response")
+    else:
+        print("We have invalied reposne")
+
+    return {
+        "check":"response not empty",
+        "reason": "Response has content"
+    }
+        
+
+
